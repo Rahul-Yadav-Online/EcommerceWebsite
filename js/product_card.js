26a60377147f3180b9d087f54b37d9e2f0fd1d53
Rahul-Yadav-Online <ryonline.work@gmail.com>
2023-05-11 23:17:04 +0530

    completed till task 6

--- js/product_card.js
+++ js/product_card.js
@@ -7,7 +7,7 @@ function cardGroupBottom(){
 }
 
 function card(imageUrl, productName, productPrice=0){
-    return '<div class="col-4"><div class="ucard"><a href="#"> \
+    return '<div class="col-12 col-md-4"><div class="ucard"><a href="#"> \
     <img class="d-block" src="'+imageUrl+'" alt="" width="100%" height="400px"></a> \
     <div class="hov"><h5 class="d-flex justify-content-center mb-0 pb-2">Add to Cart</h5>\
     </div></div><h4 class="d-flex justify-content-center my-2">'+productName+'</h4> \
