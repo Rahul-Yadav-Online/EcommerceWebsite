537627b01bd26bc35117fd4e41b09400929807f2
Rahul-Yadav-Online <ryonline.work@gmail.com>
2023-05-11 17:02:14 +0530

    front-end all pages added

--- js/products_db.js
+++ js/products_db.js
@@ -1,14 +1,42 @@
 class Products{
-    constructor(name, price, description, imageUrl){
+    constructor(name='', price=0, description='', imageUrl=''){
         this._name = name;
         this._price = price;
         this._description = description;
-        this._imageurl = imageUrl;
+        this._imageUrl = imageUrl;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    get price(){
+        return this._price;
+    }
+
+    get description(){
+        return this._description;
+    }
+
+    get imageUrl(){
+        return this._imageUrl;
     }
 }
 
 class ProductsList{
-    constructor(productsArray){
+    constructor(productsArray=[]){
         this._products = productsArray;
+    }
+
+    get products(){
+        return this._products;
+    }
+
+    addProduct(product){
+        this._products.push();
+    }
+
+    getProductString(){
+        return JSON.stringify(this._products); 
     }
 }
